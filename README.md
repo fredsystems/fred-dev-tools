2e9dff1eef90989f73398f9e64a2d33aae5aa922
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2026-01-05 12:49:10 -0700

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # fred-dev-tools
- 
---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "Personal developer toolbox devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    precommit-base = {
+      url = "github:FredSystems/pre-commit-checks";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      precommit-base,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        preCommit = precommit-base.lib.mkCheck {
+          inherit system;
+          src = ./.;
+
+          extraExcludes = [
+            "secrets.yaml"
+            "tsconfig.json"
+          ];
+        };
+      in
+      {
+        # --- checks must be an attrset ---
+        checks = {
+          pre-commit-check = preCommit;
+        };
+
+        # --- devShell must be a derivation ---
+        devShells.default = pkgs.mkShell {
+          name = "dev-tools";
+
+          packages = with pkgs; [
+            clang
+            gcc
+            hadolint
+            gnumake
+            cmake
+            ninja
+            shellcheck
+            typos
+
+            jq
+            yq
+
+            pkg-config
+
+            rust-bin.stable.latest.default
+
+            nodePackages_latest.nodejs
+            nodePackages.prettier
+            npm-check
+
+            python3
+          ];
+
+          shellHook = ''
+            echo "🧰 Entered dev-tools shell for ${system}"
+            ${preCommit.shellHook}
+          '';
+        };
+      }
+    );
+}
